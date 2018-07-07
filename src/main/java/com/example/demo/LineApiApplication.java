22c6c0315b1038d6e49295800bbe20bed64434d5
MichiOnoma <m.3iche@gmail.com>
2018-07-07 23:52:52 +0900

    first commit

---
+++ src/main/java/com/example/demo/LineApiApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+//@LineMessageHandler
+@RestController
+public class LineApiApplication {
+
+	@GetMapping
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/LineApiConst.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public final class LineApiConst {
+
+	public static class MESSAGE {
+		public static final String KAKKO1 = "「";
+		public static final String KAKKO2 = "」";
+		public static final String CONFIRM_MSG = "について保存しますか参照しますか。";
+	}
+
+	public static class BUTTON {
+		public static final String SAVE = "保存";
+		public static final String REF = "参照";
+		public static final String YES = "はい";
+		public static final String NO = "いいえ";
+	}
+
+	public static class VAL {
+		public static final int SHELF_LIFE = 7;
+	}
+}
