a3c47faf363fff8279e9dc1daddc0ad8fbfa1a28
MichiOnoma <m.3iche@gmail.com>
2018-07-20 22:15:11 +0900

    修正

--- src/main/java/com/example/demo/LineApiApplication.java
+++ src/main/java/com/example/demo/LineApiApplication.java
@@ -2,14 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
 
+
 @SpringBootApplication
 @LineMessageHandler
 @RestController
+@EntityScan(basePackageClasses = {LineApiApplication.class, Jsr310Converters.class})
 public class LineApiApplication {
 
 	@GetMapping
